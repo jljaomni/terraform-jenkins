bc2e14d6a86dad821d61344f8bdb66eba932b4e4
jljulioa <jljulioa@gmail.com>
2024-08-20 23:44:50 -0500

    terraform_edit

--- backend.tf
+++ backend.tf
@@ -1,31 +1,4 @@
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "terraform-state-bucket-jenkins" 
-  versioning {
-    enabled = true
-  }
-
-  tags = {
-    Name        = "Terraform State Bucket"
-    Environment = "Dev"
-  }
-}
-
-resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "terraform-locks"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-
-  tags = {
-    Name        = "Terraform Locks Table"
-    Environment = "Dev"
-  }
-}
-
+/*
 terraform {
   backend "s3" {
     bucket         = "terraform-state-bucket-jenkins"  
@@ -35,3 +8,4 @@ terraform {
     encrypt        = true 
   }
 }
+*/
--- main.tf
+++ main.tf
@@ -1,65 +1,77 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "~> 5.0"
+module "ecs" {
+  source  = "terraform-aws-modules/ecs/aws"
+  version = "5.11.4"
+
+  cluster_name = "ecs-integrated"
+
+  cluster_configuration = {
+    execute_command_configuration = {
+      logging = "OVERRIDE"
+      log_configuration = {
+        cloud_watch_log_group_name = "/aws/ecs/aws-ec2"
+      }
     }
   }
-}
 
-provider "aws" {
-  region     = "us-east-1"
-  access_key = var.access_key
-  secret_key = var.secret_key
-}
-locals {
-  extra_tag = "extra-tag"
-}
+  fargate_capacity_providers = {
+    FARGATE = {
+      default_capacity_provider_strategy = {
+        weight = 50
+      }
+    }
+    FARGATE_SPOT = {
+      default_capacity_provider_strategy = {
+        weight = 50
+      }
+    }
+  }
 
-module "ecs" {
-  source = "terraform-aws-modules/ecs/aws"
+  services = {
+    jenkins-service = {
+      cpu    = 1024
+      memory = 4096
 
-  name = "jenkins-ecs-cluster"
-  cluster_name = "jenkins-cluster"
-
-  vpc_id  = module.vpc.vpc_id
-  subnets = module.vpc.public_subnets
-
-  services = [
-    {
-      name = "jenkins-service"
-      task_definition = {
-        container_definitions = jsonencode([{
-          name      = "jenkins"
-          image     = "jenkins/jenkins:lts"
-          cpu       = 256
-          memory    = 512
+      # Container definition(s)
+      container_definitions = {
+        jenkins = {
+          cpu       = 512
+          memory    = 1024
           essential = true
-          portMappings = [{
-            containerPort = 8080
-            hostPort      = 8080
-            protocol      = "tcp"
-          }]
-        }])
-        family                   = "jenkins-task"
-        requires_compatibilities = ["FARGATE"]
-        cpu                      = 512
-        memory                   = 1024
-        network_mode             = "awsvpc"
-      }
-      desired_count = 2
-      launch_type   = "FARGATE"
-      network_configuration = {
-        security_groups = [module.terraform-sg.security_group_id]
+          image     = "jenkins/jenkins:lts"
+          port_mappings = [
+            {
+              containerPort = 80
+              protocol      = "tcp"
+            }
+          ]
+          readonly_root_filesystem = false
+         
+          service_connect_configuration = {
+            namespace = "jenkins-namespace"
+            service = {
+              client_alias = {
+                port     = 80
+                dns_name = "ecs-jenkins-service"
+              }
+              port_name      = "ecs-jenkins-service"
+              discovery_name = "ecs-jenkins-service"
+            }
+          }
+        }
       }
 
-      load_balancer = {
-        target_group_arn = module.alb.target_group_arns[0]
-        container_name   = "jenkins"
-        container_port   = 8080
-      }
+      /*  load_balancer = {
+        service = {
+          target_group_arn = [module.alb.target_groups]
+          container_name   = "jenkins"
+          container_port   = 80
+        }
+      } */
 
-      # Auto Scaling Configuration
+      subnet_ids = module.vpc.public_subnets
+
+      security_groups = [aws_security_group.ecs_service_sg.id]
+
       autoscaling = {
         min_capacity = 1
         max_capacity = 3
@@ -68,11 +80,12 @@ module "ecs" {
         scale_in_cooldown      = 300
         scale_out_cooldown     = 300
       }
+
     }
-  ]
+  }
 
   tags = {
-    Environment = "dev"
-    Terraform   = "true"
+    Environment = "Development"
+    Project     = "Example"
   }
 }
