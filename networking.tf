2301e2e03a3fea80a1071cd18332d42fa4c718fd
jljulioa <jljulioa@gmail.com>
2024-08-20 23:44:50 -0500

    terraform_edit

--- networking.tf
+++ networking.tf
@@ -1,5 +1,6 @@
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
+  version = "5.13.0"
 
   name = "terraform-vpc"
   cidr = "10.0.0.0/16"
@@ -17,7 +18,7 @@ module "vpc" {
   }
 }
 
-module "terraform-sg" {
+/* module "terraform-sg" {
   source = "terraform-aws-modules/security-group/aws"
 
   name                = "terraform-sg"
@@ -27,4 +28,35 @@ module "terraform-sg" {
   ingress_rules       = ["http-80-tcp", "https-443-tcp"]
   egress_cidr_blocks  = ["0.0.0.0/0"]
   egress_rules        = ["https-443-tcp"]
+} */
+
+
+resource "aws_security_group" "ecs_service_sg" {
+  name        = "ecs_service_sg"
+  description = "Security group for ECS service"
+  vpc_id      = module.vpc.vpc_id
+
+  tags = {
+    Name = "ecs_service_sg"
+  }
+}
+
+resource "aws_security_group_rule" "allow_alb" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  description       = "Allow inbound traffic from ALB to ECS service"
+  security_group_id = aws_security_group.ecs_service_sg.id
+}
+
+resource "aws_security_group_rule" "allow_egress" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  description       = "Allow all outbound traffic from ECS service"
+  security_group_id = aws_security_group.ecs_service_sg.id
 }
--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,17 @@
-output "jenkins_alb_dns_name" {
-  description = "The DNS name of the Jenkins ALB"
-  value       = aws_lb.jenkins_alb.dns_name
+/*
+output "trfm_alb_dns_name" {
+  value       = module.alb.dns_name
+  description = "The DNS name of the ALB"
 }
+
+
+*/
+output "trfm_public_subnet_ids" {
+  value       = [module.vpc.public_subnets]
+  description = "List of public subnet IDs"
+}
+
+/* output "alb_target_group_arn" {
+  description = "ALB Target Group ARN"
+  value       = module.alb.target_groups[0]
+} */
