58ebc34e52d0a3e381f4385996df578aa877a491
Jaime Julio <jaime.julio@omni.pro>
2024-09-12 09:28:34 -0500

    update

--- nat_instance.tf
+++ nat_instance.tf
@@ -1,20 +1,14 @@
-# Crear NAT Instances en todas las subnets públicas usando for_each
+/* # Crear NAT Instances en todas las subnets públicas usando for_each
 resource "aws_instance" "nat_instance" {
-  for_each               = toset(module.vpc.public_subnets) # Recorre todas las subnets públicas
-  ami                    = "ami-02c21308fed24a8ab"           # AMI de Amazon Linux 2, puedes ajustar esta AMI según tu región
+  ami                    = "ami-0cdfb12db83f3c627"           # AMI de Amazon NAT
   instance_type          = "t3.micro"
-  subnet_id              = each.value                        # Asigna la subnet pública
+  subnet_id              = module.vpc.public_subnets[0]                        # Asigna la subnet pública
   associate_public_ip_address = true
-  security_groups        = [aws_security_group.nat_sg.id]
-
-  # Configuración para habilitar el reenvío de IPs
-  user_data = <<-EOF
-              #!/bin/bash
-              sudo sysctl -w net.ipv4.ip_forward=1
-              sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-              EOF
+  security_groups        = [aws_security_group.nat_instance_sg.id]
+  source_dest_check      = false
 
   tags = {
-    Name = "NAT Instance ${each.key}"
+    Name = "NAT Instance"
   }
 }
+  */
--- networking.tf
+++ networking.tf
@@ -9,7 +9,7 @@ module "vpc" {
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnets = ["10.0.101.0/24", "10.0.102.0/24"]
 
-  enable_nat_gateway   = true
+  enable_nat_gateway   = false
   enable_vpn_gateway   = true
   enable_dns_hostnames = true
   enable_dns_support   = true
@@ -111,3 +111,54 @@ resource "aws_security_group" "alb_sg" {
     Name = "alb-sg"
   }
 }
+
+/* resource "aws_security_group" "nat_instance_sg" {
+  vpc_id = module.vpc.vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+
+  ingress {
+    from_port   = 9000
+    to_port     = 9000
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "nat-instance-sg"
+  }
+}
+ */
